7b7955f86490c84c3b912180fba5fb45c385d112
Ali <tu8cvbxnx@code.edu.az>
2022-05-23 12:51:31 +0400

    Initial commit

---
+++ BackEnd e basladim insAllah yaxsi oyrenerem/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd_e_basladim_insAllah_yaxsi_oyrenerem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Product = "Tutku";
+            ViewData["Count"] = 20;
+            TempData["Price"] = "0.9";
+            return RedirectToAction("About");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ(string name,string surname)
+        {
+            string fullname = $"{name} {surname}";
+            return Content(fullname);
+        }
+    }
+}
